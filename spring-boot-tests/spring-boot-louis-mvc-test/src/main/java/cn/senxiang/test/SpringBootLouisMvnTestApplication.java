b047c5cbc9e3571695e724ac7a070c27683e7e04
灬_月_灬 <178508542@qq.com>
2019-02-15 17:50:23 +0800

    新增个人测试模块

---
+++ spring-boot-tests/spring-boot-louis-mvc-test/src/main/java/cn/senxiang/test/SpringBootLouisMvnTestApplication.java
@@ -0,0 +1,14 @@
+package cn.senxiang.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLouisMvnTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLouisMvnTestApplication.class, args);
+	}
+
+}
+
